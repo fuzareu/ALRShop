b5a624978dcf93c1d91f86ac6083253eed31aa08
AvaRiZ <jaynielacusapernecita@gmail.com>
2025-04-20 15:07:43 +0800

    inngest

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/user";
+import Order from "@/models/order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
