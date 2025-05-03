d993b80763ebc4183f2bd2d9957c081c90ea0fa4
Fuzaru <fuzareu@gmail.com>
2025-05-03 13:14:57 +0800

    edited payment logo

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { gcash_logo } from "@/assets/assets";
+
 
 const OrderSummary = () => {
 
@@ -213,7 +215,7 @@ const OrderSummary = () => {
               On-site Payment
             </button>
             <button onClick={createOrderStripe} className="w-full-flex justify-center items-center border border-indigo-500 bg-white hover:bg-gray-100 py-2 mt-5">
-              <Image className="w-12" src={assets.gcash_logo} alt=""/>
+              <Image className="w-12" src={gcash_logo} alt=""/>
             </button>
           </div>
         )
