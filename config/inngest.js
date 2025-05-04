2b0cd061c3168937e56407fe21f3540e39027ea7
Fuzaru <fuzareu@gmail.com>
2025-05-05 03:31:38 +0800

    undo db rename

--- config/inngest.js
+++ config/inngest.js
@@ -4,7 +4,7 @@ import User from "@/models/user";
 import Order from "@/models/order";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ALRShop-next" });
+export const inngest = new Inngest({ id: "quickcart-next" });
 
 // Inggest Function save user data to MongoDB
 export const syncUserCreation = inngest.createFunction(
